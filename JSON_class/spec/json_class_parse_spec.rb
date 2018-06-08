fa7b3555fdc9a944a5602c3d32ae6fc1f93f83e7
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-06-08 10:47:39 +0100

    All tests for json_class_parse.rb passed.

--- JSON_class/spec/json_class_parse_spec.rb
+++ JSON_class/spec/json_class_parse_spec.rb
@@ -8,4 +8,22 @@ describe 'Testing the exchange rates' do
   it 'Should be a hash' do
     expect(@exchange_rates.json_file).to be_kind_of(Hash)
   end
+
+  it 'Should contain the base as EUR' do
+    expect(@exchange_rates.json_file['base']).to include("EUR")
+  end
+
+  it 'Should have a date string' do
+    expect(@exchange_rates.json_file['date']).to be_kind_of(String)
+  end
+
+  it 'Should have 31 rates' do
+    expect(@exchange_rates.json_file['rates'].keys.count).to eq(31)
+  end
+
+  it 'Should have all rates as floats' do
+    @exchange_rates.json_file['rates'].each do |key, value|
+      expect(value).to be_kind_of(Float)
+    end
+  end
 end
